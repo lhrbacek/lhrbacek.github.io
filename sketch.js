3db9cd69fc17370566051d5eebffffd2e19a4e28
Lubomir Hrbacek <xhrbace1@fi.muni.cz>
2024-12-17 18:14:40 +0100

    Add project

---
+++ sketch.js
@@ -0,0 +1,201 @@
+function loadRoutes(routesData) {
+  let minx = Infinity;
+  let miny = Infinity;
+  let maxx = -Infinity;
+  let maxy = -Infinity;
+
+  const routesParsed = JSON.parse(routesData[0]);
+
+  // Iterate through each feature
+  routesParsed.features.forEach(feature => {
+    const nazLinky = feature.properties.naz_linky; // Get 'naz_linky'
+    const geometryType = feature.geometry.type; // Get geometry type
+    const coordinates = feature.geometry.coordinates; // Get coordinates
+
+    console.log(`naz_linky: ${nazLinky}`);
+    console.log(`Geometry Type: ${geometryType}`);
+
+    fill(50);
+
+    if (geometryType === "LineString") {
+      //console.log('Coordinates:');
+      coordinates.forEach(pair => {
+        //console.log(`  [${pair[0]}, ${pair[1]}]`);
+        let pairCanvas = coordsToCanvas(pair);
+        circle(pairCanvas[0], pairCanvas[1],5);
+        if (pair[0] < minx) minx = pair[0];
+        if (pair[0] > maxx) maxx = pair[0];
+        if (pair[1] < miny) miny = pair[1];
+        if (pair[1] > maxy) maxy = pair[1];
+      });
+    } else if (geometryType === "MultiLineString") {
+      //console.log('Coordinates:');
+      coordinates.forEach(line => {
+        //console.log('  Line:');
+        line.forEach(pair => {
+          //console.log(`    [${pair[0]}, ${pair[1]}]`);
+          let pairCanvas = coordsToCanvas(pair);
+          circle(pairCanvas[0], pairCanvas[1],5);
+          //console.log(`${pairCanvas[0]}, ${pairCanvas[1]}`);
+        });
+      });
+    } else {
+      console.log(`Unknown geometry type: ${geometryType}`);
+    }
+  });
+
+  console.log(`minx: ${minx}`);
+  console.log(`maxx: ${maxx}`);
+  console.log(`miny: ${miny}`);
+  console.log(`maxy: ${maxy}`);
+}
+
+// send msg after connecting to socket
+function sendIntro() {
+  // send filter to receive only trams
+  socket.send('{"filter":{"where":"vtype=0"}}');
+}
+
+// react to received msg from socket
+function readMessage(event) {
+  var msg = event.data;
+  const tramsParsed = JSON.parse(msg);
+  if (!tramsParsed.geometry) {
+    console.log('not trams, maybe filter response');
+    return;
+  }
+
+  if (tramsParsed.attributes.lng < boundariesLng[0] || tramsParsed.attributes.lng > boundariesLng[1] || 
+      tramsParsed.attributes.lat < boundariesLat[0] || tramsParsed.attributes.lat > boundariesLat[1]) {
+    //console.log(`tram ${tramsParsed.attributes.lineid}: ${tramsParsed.attributes.lng}, ${tramsParsed.attributes.lat}`);
+    //console.log('outside boundaries: 16.507-16.693, 49.126-49.248');
+    return;
+  }
+
+  let pairCanvas = coordsToCanvas([tramsParsed.attributes.lng, tramsParsed.attributes.lat]);
+
+  if (!updateTram(tramsParsed.attributes.id, pairCanvas)) {
+    // if (tramsid.indexOf(tramsParsed.attributes.id) !== -1) {
+    //   console.log(`Duplicate tramid: ${tramsParsed.attributes.id}`);
+    // }
+    //tramsid.push(tramsParsed.attributes.id);
+    trams.push(new Tram(tramsParsed.attributes.id, tramsParsed.attributes.lineid, tramsParsed.attributes.linename, pairCanvas));
+    //seenTrams.add(tramsParsed.attributes.lineid);
+  }
+}
+
+function coordsToCanvas(coords) {
+  let x = map(coords[0], 16.50, 16.70, 0, canvasWidth, true);
+  let y = map(coords[1], 49.125, 49.25, canvasHeight, 0, true);
+  return [floor(x), floor(y)];
+}
+
+// update tram after new coords are received
+function updateTram(id, coords) {
+  for(const tram of trams) {
+    if (tram.id === id) {
+      tram.updateCoords(coords);
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkSleepingTrams() {
+
+  for(const tram of trams) {
+    if (!tram.isSleeping && tram.lastUpdateFrame + 5400 < frameCount) { // inactive for 3 mins
+      console.log(`Sleeping: ${tram.lineid}`);
+      tram.traces = [];
+      tram.isSleeping = true;
+    }
+  }
+  //save(`heartbeat_of_brno_${img_count}`);
+  //img_count++;
+}
+
+// const colors = new Map([
+//   [1, '#FF0202'],
+//   [2, '#FF1717'],
+//   [3, '#FF2C2C'],
+//   [4, '#FF4141'],
+//   [5, '#FF5656'],
+//   [6, '#FF6B6B'],
+//   [7, '#FF8080'],
+//   [8, '#FF9595'],
+//   [9, '#FFAAAA'],
+//   [10, '#FFBFBF'],
+//   [11, '#FFD4D4'],
+//   [12, '#FFE9E9'],
+// ]);
+
+const rgbColor = new Map([
+  [1, 0],
+  [2, 15],
+  [3, 30],
+  [4, 45],
+  [5, 60],
+  [6, 75],
+  [7, 90],
+  [8, 105],
+  [9, 120],
+  [10, 135],
+  [11, 150],
+  [12, 165],
+]);
+
+let fr;
+let canvasWidth = 800;
+let canvasHeight = 800;
+let host = 'gis.brno.cz/geoevent/ws/services/ODAE_public_transit_stream/StreamServer/subscribe?outSR=4326';
+let socket; // the websocket
+let routesGeoJSON;
+let boundariesLat = [49.126, 49.248]
+let boundariesLng = [16.507, 16.693]
+let trams = [];
+let tramsid = [];
+let img_count = 0;
+
+// Load the text and create an array. TODO remove when not needed
+function preload() {
+  //routesGeoJSON = loadStrings('./files/transit_routes.geojson');
+}
+
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  frameRate(30);
+
+  //traceLayer = createGraphics(width, height);
+  //traceLayer.background(20, 20, 20);
+  //traceLayer.noStroke();
+
+  //loadRoutes(routesGeoJSON);
+
+  // connect to server:
+  socket = new WebSocket('wss://' + host);
+  // socket connection listener:
+  socket.onopen = sendIntro;
+  // socket message listener:
+  socket.onmessage = readMessage;
+
+  fr = createP('');
+}
+
+function draw() {
+  background(20, 20, 20);
+  //traceLayer.background(20, 20, 20);
+  for (let tram of trams) {
+    tram.showTraces();
+    tram.show();
+  }
+  //image(traceLayer, 0, 0);
+  // for (let tram of trams) {
+  //   tram.show();
+  // }
+  
+  if (frameCount % 5400 === 0) {
+    console.log(`trams amount: ${trams.length}`)
+    checkSleepingTrams();
+  }
+  fr.html(floor(frameRate()));
+}
---
+++ tram.js
@@ -0,0 +1,135 @@
+class Tram {
+  constructor(id, lineid, lineName, coords) {
+    this.id = id;
+    this.lineid = lineid;
+    this.lineName = lineName;
+    this.lastCoordsCanvas = coords;
+    this.currCoordsCanvas = coords;
+    this.lastCoordsDrawn = coords;
+    this.intervalFrameCount = frameCount + 300;
+    this.lastUpdateFrame = frameCount;
+    this.lastDrawnAngle = 0;
+    this.traces = [];
+    this.isSleeping = true;
+  }
+
+  updateCoords(coords) {
+    if(coords[0] === this.currCoordsCanvas[0] && coords[1] === this.currCoordsCanvas[1]) {
+      return;
+    }
+    // if (abs(coords[0] - this.lastCoordsCanvas[0]) > 50 || abs(coords[1] - this.lastCoordsCanvas[1]) > 50) {
+    //   console.log(`${this.lineid}; old: ${this.lastCoordsCanvas[0]},${this.lastCoordsCanvas[1]}; new: ${coords[0]},${coords[1]}`);
+    // }
+    this.lastCoordsCanvas = this.currCoordsCanvas;
+    this.currCoordsCanvas = coords;
+    this.lastUpdateFrame = frameCount;
+    this.updateIntervalFrameCount();
+    this.isSleeping = false;
+  }
+
+  updateIntervalFrameCount() {
+    this.intervalFrameCount = frameCount + 300; // 10 seconds
+  }
+
+  showTraces() {
+    //traceLayer.beginShape();
+    beginShape();
+    if (this.traces.length > 0)
+      //traceLayer.curveVertex(this.traces[0].x, this.traces[0].y);
+      curveVertex(this.traces[0].x, this.traces[0].y);
+    for (let i = 0; i < this.traces.length; i += 50) {
+      //traceLayer.curveVertex(this.traces[i].x, this.traces[i].y);
+      curveVertex(this.traces[i].x, this.traces[i].y);
+    }
+    let clr = rgbColor.get(this.lineid);
+    if (clr === undefined)
+      clr = 180;
+    // traceLayer.noFill();
+    // traceLayer.stroke(210,clr,clr,255);
+    // traceLayer.strokeWeight(3);
+    // traceLayer.endShape();
+    noFill();
+    stroke(210,clr,clr,255);
+    strokeWeight(3);
+    endShape();
+  }
+
+  inCenter(coords) {
+    return dist(440, 350, coords[0], coords[1]) <= 50;
+  }
+
+  heartbeatPulse(frame) {
+    let beatLength = 45; // Total frames for one heartbeat cycle (e.g., 1 second at 60 FPS)
+    let phase = frame % beatLength; // Current frame within the cycle
+    
+    // Heartbeat pattern: quick pulse, short delay, second pulse, long delay
+    if (phase < 5) {
+      // First quick beat (frames 0–5)
+      return map(phase, 0, 5, 30, 80); // Rising to maximum
+    } else if (phase < 10) {
+      // Dip after the first beat (frames 5–10)
+      return map(phase, 5, 10, 80, 40); // Falling to a medium level
+    } else if (phase < 15) {
+      // Second beat (frames 10–15)
+      return map(phase, 10, 15, 40, 70); // Rising again
+    } else if (phase < 20) {
+      // Dip after the second beat (frames 5–10)
+      return map(phase, 15, 20, 70, 30); // Falling to a medium level
+    } else {
+      // Resting period (frames 15–25)
+      return 30; // Back to baseline
+    }
+  }
+
+  show() {
+    // Calculate normalized progress after the delay
+    let t = constrain((frameCount - this.intervalFrameCount + 300) / 300, 0, 1);
+
+    // Calculate the current position
+    let x = lerp(this.lastCoordsCanvas[0], this.currCoordsCanvas[0], t);
+    let y = lerp(this.lastCoordsCanvas[1], this.currCoordsCanvas[1], t);
+
+    let clr = rgbColor.get(this.lineid);
+    if (clr === undefined)
+      clr = 180;
+
+    if (this.lastCoordsDrawn[0] !== x || this.lastCoordsDrawn[1] !== y) {
+      this.traces.push({x: x, y: y});
+      this.lastCoordsDrawn = [x, y];
+    }
+
+    let angle = atan2(this.currCoordsCanvas[1] - this.lastCoordsCanvas[1], this.currCoordsCanvas[0] - this.lastCoordsCanvas[0]);
+    if (angle === 0) {
+      angle = this.lastDrawnAngle;
+    }
+    this.lastDrawnAngle = angle;
+    
+    let pulse = 25
+    let minPulse = 4;
+    let maxPulse = 20;
+    let stepPulse = 2;
+    if (!this.isSleeping) {
+      push();
+      translate(x, y); // Move to the tram's position
+
+      if (this.inCenter([x, y])) {
+        pulse = this.heartbeatPulse(frameCount);
+        minPulse = 8;
+        maxPulse = 40;
+        stepPulse = 3;
+      }
+      for (let i = minPulse; i <= maxPulse; i += stepPulse) { // Layers of the glow
+        fill(220, 180, 180, map(i, 4, pulse, 60, 0));
+        noStroke();
+        ellipse(0, 0, i, i); // Expanding circle
+      }
+
+      rotate(angle);   // Rotate to match the direction
+      fill(180, 50, 50); // Red tram
+      noStroke();
+      // Draw a triangle pointing right
+      triangle(-5, -5, -5, 5, 7, 0); // Adjust size as needed
+      pop();
+    }
+  }
+}
