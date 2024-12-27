5308e7eee683b7619e3bc2b40aa20a5006ef26ee
Lubomir Hrbacek <xhrbace1@fi.muni.cz>
2024-12-27 13:33:22 +0100

    Clean the code

--- sketch.js
+++ sketch.js
@@ -1,54 +1,6 @@
-function loadRoutes(routesData) {
-  let minx = Infinity;
-  let miny = Infinity;
-  let maxx = -Infinity;
-  let maxy = -Infinity;
-
-  const routesParsed = JSON.parse(routesData[0]);
-
-  // Iterate through each feature
-  routesParsed.features.forEach(feature => {
-    const nazLinky = feature.properties.naz_linky; // Get 'naz_linky'
-    const geometryType = feature.geometry.type; // Get geometry type
-    const coordinates = feature.geometry.coordinates; // Get coordinates
-
-    console.log(`naz_linky: ${nazLinky}`);
-    console.log(`Geometry Type: ${geometryType}`);
-
-    fill(50);
-
-    if (geometryType === "LineString") {
-      //console.log('Coordinates:');
-      coordinates.forEach(pair => {
-        //console.log(`  [${pair[0]}, ${pair[1]}]`);
-        let pairCanvas = coordsToCanvas(pair);
-        circle(pairCanvas[0], pairCanvas[1],5);
-        if (pair[0] < minx) minx = pair[0];
-        if (pair[0] > maxx) maxx = pair[0];
-        if (pair[1] < miny) miny = pair[1];
-        if (pair[1] > maxy) maxy = pair[1];
-      });
-    } else if (geometryType === "MultiLineString") {
-      //console.log('Coordinates:');
-      coordinates.forEach(line => {
-        //console.log('  Line:');
-        line.forEach(pair => {
-          //console.log(`    [${pair[0]}, ${pair[1]}]`);
-          let pairCanvas = coordsToCanvas(pair);
-          circle(pairCanvas[0], pairCanvas[1],5);
-          //console.log(`${pairCanvas[0]}, ${pairCanvas[1]}`);
-        });
-      });
-    } else {
-      console.log(`Unknown geometry type: ${geometryType}`);
-    }
-  });
-
-  console.log(`minx: ${minx}`);
-  console.log(`maxx: ${maxx}`);
-  console.log(`miny: ${miny}`);
-  console.log(`maxy: ${maxy}`);
-}
+// This project uses API for live tram GPS from links below, which is under CC BY 4.0 license.
+// https://gis.brno.cz/esri/home/item.html?id=e8aa121910df41bb9a28e4ca34a263c7
+// https://data.brno.cz/datasets/mestobrno::polohy-vozidel-hromadn%C3%A9-dopravy-public-transit-positional-data/about
 
 // send msg after connecting to socket
 function sendIntro() {
@@ -65,22 +17,16 @@ function readMessage(event) {
     return;
   }
 
+  // dont process trams outside bounds
   if (tramsParsed.attributes.lng < boundariesLng[0] || tramsParsed.attributes.lng > boundariesLng[1] || 
       tramsParsed.attributes.lat < boundariesLat[0] || tramsParsed.attributes.lat > boundariesLat[1]) {
-    //console.log(`tram ${tramsParsed.attributes.lineid}: ${tramsParsed.attributes.lng}, ${tramsParsed.attributes.lat}`);
-    //console.log('outside boundaries: 16.507-16.693, 49.126-49.248');
     return;
   }
 
   let pairCanvas = coordsToCanvas([tramsParsed.attributes.lng, tramsParsed.attributes.lat]);
 
   if (!updateTram(tramsParsed.attributes.id, pairCanvas)) {
-    // if (tramsid.indexOf(tramsParsed.attributes.id) !== -1) {
-    //   console.log(`Duplicate tramid: ${tramsParsed.attributes.id}`);
-    // }
-    //tramsid.push(tramsParsed.attributes.id);
     trams.push(new Tram(tramsParsed.attributes.id, tramsParsed.attributes.lineid, tramsParsed.attributes.linename, pairCanvas));
-    //seenTrams.add(tramsParsed.attributes.lineid);
   }
 }
 
@@ -98,11 +44,10 @@ function updateTram(id, coords) {
       return true;
     }
   }
-  return false;
+  return false; // tram not found, probably new one
 }
 
 function checkSleepingTrams() {
-
   for(const tram of trams) {
     if (!tram.isSleeping && tram.lastUpdateFrame + 5400 < frameCount) { // inactive for 3 mins
       console.log(`Sleeping: ${tram.lineid}`);
@@ -110,8 +55,6 @@ function checkSleepingTrams() {
       tram.isSleeping = true;
     }
   }
-  //save(`heartbeat_of_brno_${img_count}`);
-  //img_count++;
 }
 
 function setMappedBorders() {
@@ -121,26 +64,12 @@ function setMappedBorders() {
   mappedHeightMax = canvasHeight / 2 + mapHeight /2;
 }
 
-function intro() {
+function loading() {
   trams.push(new Tram(0, 0, 0, [0,500]));
   updateTram(0, [canvasWidth,500]);
 }
 
-// const colors = new Map([
-//   [1, '#FF0202'],
-//   [2, '#FF1717'],
-//   [3, '#FF2C2C'],
-//   [4, '#FF4141'],
-//   [5, '#FF5656'],
-//   [6, '#FF6B6B'],
-//   [7, '#FF8080'],
-//   [8, '#FF9595'],
-//   [9, '#FFAAAA'],
-//   [10, '#FFBFBF'],
-//   [11, '#FFD4D4'],
-//   [12, '#FFE9E9'],
-// ]);
-
+// set red brightness for each line number
 const rgbColor = new Map([
   [1, 0],
   [2, 15],
@@ -165,7 +94,7 @@ let mappedWidthMin = 0;
 let mappedHeightMin = 0;
 let mappedWidthMax = 0;
 let mappedHeightMax = 0;
-let host = 'gis.brno.cz/geoevent/ws/services/ODAE_public_transit_stream/StreamServer/subscribe?outSR=4326';
+let host = 'gis.brno.cz/geoevent/ws/services/ODAE_public_transit_stream/StreamServer/subscribe?outSR=4326'; // API for trams
 let socket; // the websocket
 let routesGeoJSON;
 let boundariesLat = [49.126, 49.248]
@@ -175,47 +104,33 @@ let tramsid = [];
 let img_count = 0;
 let centerCoords = [];
 
-// Load the text and create an array. TODO remove when not needed
-function preload() {
-  //routesGeoJSON = loadStrings('./files/transit_routes.geojson');
-}
-
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
   frameRate(30);
   setMappedBorders();
-  centerCoords = [mapWidth * 0.55 + mappedWidthMin, mapHeight * 0.45 + mappedHeightMin];
+  centerCoords = [mapWidth * 0.55 + mappedWidthMin, mapHeight * 0.45 + mappedHeightMin]; // city center coords
 
-  //traceLayer = createGraphics(width, height);
-  //traceLayer.background(20, 20, 20);
-  //traceLayer.noStroke();
-
-  //loadRoutes(routesGeoJSON);
-
-  // connect to server:
+  // connect to server
   socket = new WebSocket('wss://' + host);
-  // socket connection listener:
+  // socket connection listener
   socket.onopen = sendIntro;
-  // socket message listener:
+  // socket message listener
   socket.onmessage = readMessage;
 
   //fr = createP('');
-  intro();
+  loading();
 }
 
 function draw() {
   background(0);
-  //traceLayer.background(20, 20, 20);
   for (let tram of trams) {
     tram.showTraces();
-    //tram.show();
   }
-  //image(traceLayer, 0, 0);
   for (let tram of trams) {
     tram.show();
   }
   
-  if (frameCount % 5400 === 0) {
+  if (frameCount % 5400 === 0) { // remove sleeping trams each 180 seconds
     console.log(`trams amount: ${trams.length}`)
     checkSleepingTrams();
   }
@@ -224,9 +139,11 @@ function draw() {
   //   save(`heartbeat_of_brno_${img_count}`);
   //   img_count++;
   // }
+
   //fr.html(floor(frameRate()));
-  //circle(centerCoords[0], centerCoords[1], 50);
-  if (frameCount === 390) {
+
+  // remove loading tram
+  if (frameCount === 450) {
     trams.shift();
   }
 }
--- tram.js
+++ tram.js
@@ -1,3 +1,7 @@
+// This project uses API for live tram GPS from links below, which is under CC BY 4.0 license.
+// https://gis.brno.cz/esri/home/item.html?id=e8aa121910df41bb9a28e4ca34a263c7
+// https://data.brno.cz/datasets/mestobrno::polohy-vozidel-hromadn%C3%A9-dopravy-public-transit-positional-data/about
+
 class Tram {
   constructor(id, lineid, lineName, coords) {
     this.id = id;
@@ -9,7 +13,6 @@ class Tram {
     this.intervalFrameCount = frameCount + 300;
     this.lastUpdateFrame = frameCount;
     this.lastDrawnAngle = 0;
-    this.lastDrawnColor = [200, 50, 50];
     this.traces = [];
     this.isSleeping = true;
   }
@@ -18,9 +21,6 @@ class Tram {
     if(coords[0] === this.currCoordsCanvas[0] && coords[1] === this.currCoordsCanvas[1]) {
       return;
     }
-    // if (abs(coords[0] - this.lastCoordsCanvas[0]) > 50 || abs(coords[1] - this.lastCoordsCanvas[1]) > 50) {
-    //   console.log(`${this.lineid}; old: ${this.lastCoordsCanvas[0]},${this.lastCoordsCanvas[1]}; new: ${coords[0]},${coords[1]}`);
-    // }
     this.lastCoordsCanvas = this.currCoordsCanvas;
     this.currCoordsCanvas = coords;
     this.lastUpdateFrame = frameCount;
@@ -33,22 +33,17 @@ class Tram {
   }
 
   showTraces() {
-    //traceLayer.beginShape();
     beginShape();
-    if (this.traces.length > 0)
-      //traceLayer.curveVertex(this.traces[0].x, this.traces[0].y);
+    if (this.traces.length > 0) {
       curveVertex(this.traces[0].x, this.traces[0].y);
+    }
     for (let i = 0; i < this.traces.length; i += 40) {
-      //traceLayer.curveVertex(this.traces[i].x, this.traces[i].y);
       curveVertex(this.traces[i].x, this.traces[i].y);
     }
     let clr = rgbColor.get(this.lineid);
-    if (clr === undefined)
+    if (clr === undefined) {
       clr = 180;
-    // traceLayer.noFill();
-    // traceLayer.stroke(210,clr,clr,255);
-    // traceLayer.strokeWeight(3);
-    // traceLayer.endShape();
+    }  
     noFill();
     stroke(210,clr,clr,255);
     strokeWeight(3);
@@ -56,7 +51,7 @@ class Tram {
   }
 
   inCenter(coords) {
-    return dist(mapWidth * 0.55 + mappedWidthMin, mapHeight * 0.45 + mappedHeightMin, coords[0], coords[1]) <= 40;
+    return 40 >= dist(mapWidth * 0.55 + mappedWidthMin, mapHeight * 0.45 + mappedHeightMin, coords[0], coords[1]);
   }
 
   heartbeatPulse(frame) {
@@ -74,10 +69,10 @@ class Tram {
       // Second beat (frames 10–15)
       return map(phase, 10, 15, 40, 70); // Rising again
     } else if (phase < 20) {
-      // Dip after the second beat (frames 5–10)
+      // Dip after the second beat (frames 15–20)
       return map(phase, 15, 20, 70, 30); // Falling to a medium level
     } else {
-      // Resting period (frames 15–25)
+      // Resting period (frames 20–45)
       return 30; // Back to baseline
     }
   }
@@ -93,22 +88,19 @@ class Tram {
     let x = lerp(this.lastCoordsCanvas[0], this.currCoordsCanvas[0], t);
     let y = lerp(this.lastCoordsCanvas[1], this.currCoordsCanvas[1], t);
 
-    let clr = rgbColor.get(this.lineid);
-    if (clr === undefined)
-      clr = 180;
-
+    // did the tram move
     if (this.lastCoordsDrawn[0] !== x || this.lastCoordsDrawn[1] !== y) {
       this.traces.push({x: x, y: y});
       this.lastCoordsDrawn = [x, y];
     }
-    
+
+    push();
+    translate(x, y); // Move to the tram's position
+
     let pulse = 25
     let minPulse = 4;
     let maxPulse = 20;
     let stepPulse = 2;
-
-    push();
-    translate(x, y); // Move to the tram's position
 
     if (this.inCenter([x, y])) {
       pulse = this.heartbeatPulse(frameCount);
@@ -116,7 +108,8 @@ class Tram {
       maxPulse = 40;
       stepPulse = 3;
     }
-    for (let i = minPulse; i <= maxPulse; i += stepPulse) { // Layers of the glow
+    // Layers of the glow
+    for (let i = minPulse; i <= maxPulse; i += stepPulse) {
       fill(220, 180, 180, map(i, 4, pulse, 60, 0));
       noStroke();
       ellipse(0, 0, i, i); // Expanding circle
@@ -127,30 +120,6 @@ class Tram {
       angleMov = this.lastDrawnAngle;
     }
     this.lastDrawnAngle = angleMov;
-    // let angleCenter = atan2(centerCoords[1] - this.lastCoordsDrawn[1], centerCoords[0] - this.lastCoordsDrawn[0]);
-    // // Determine if moving toward or away from center
-    // if (abs(angleMov - angleCenter) < PI / 2) {
-    //   fill(50, 50, 200);
-    // } else {
-    //   fill(200, 50, 50);
-    // }
-
-    // let vectorToCenter = createVector(centerCoords[0] - this.lastCoordsDrawn[0], centerCoords[1] - this.lastCoordsDrawn[1]);
-    // // Tram's movement vector
-    // let movementVector = createVector(this.currCoordsCanvas[0] - this.lastCoordsDrawn[0], this.currCoordsCanvas[1] - this.lastCoordsDrawn[1]);
-    // // Calculate the dot product
-    // let dotProduct = vectorToCenter.dot(movementVector);
-    // // Determine color based on dot product
-    // //let tramColor = dotProduct > 0 ? color(0, 0, 255) : color(255, 0, 0); // Blue (toward) or Red (away)
-    // if (dotProduct > 0) {
-    //   fill(50, 50, 200);
-    //   this.lastDrawnColor = [50,50,200];
-    // } else if (dotProduct < 0){
-    //   fill(200, 50, 50);
-    //   this.lastDrawnColor = [200,50,50];
-    // } else {
-    //   fill(this.lastDrawnColor[0], this.lastDrawnColor[1], this.lastDrawnColor[2]);
-    // }
 
     rotate(angleMov);   // Rotate to match the direction
     fill(180, 50, 50); // Red tram
